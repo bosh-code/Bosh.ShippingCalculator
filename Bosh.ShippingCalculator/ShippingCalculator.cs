8f6e159dd897f688114763cf629c5201b004f14d
bosh-code <ryan.julian.bosher@icloud.com>
2022-02-21 12:51:07 +1300

    add step 1 code, tests passing

--- Bosh.ShippingCalculator/ShippingCalculator.cs
+++ Bosh.ShippingCalculator/ShippingCalculator.cs
@@ -4,6 +4,12 @@ public static class ShippingCalculator
 {
     public static int GetPrice(int size)
     {
-        return size;
+        return size switch
+        {
+            < 10 => 3,
+            < 50 => 8,
+            < 100 => 15,
+            >= 100 => 25
+        };
     }
 }
