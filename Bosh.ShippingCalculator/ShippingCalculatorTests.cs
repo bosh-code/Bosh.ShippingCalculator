18e8f6b09b633b256762525f609cea3802f532f0
bosh-code <ryan.julian.bosher@icloud.com>
2022-02-21 12:50:12 +1300

    add failing act and assert tests for Step 1

--- Bosh.ShippingCalculator/ShippingCalculatorTests.cs
+++ Bosh.ShippingCalculator/ShippingCalculatorTests.cs
@@ -7,7 +7,16 @@ public class ShippingCalculatorTests
     [Test]
     public void GetPrice()
     {
-        var result = ShippingCalculator.GetPrice(1);
-        Assert.AreEqual(1, result);
+        // Act
+        var small = ShippingCalculator.GetPrice(6);
+        var medium = ShippingCalculator.GetPrice(42);
+        var large = ShippingCalculator.GetPrice(75);
+        var xl = ShippingCalculator.GetPrice(101);
+
+        // Assert
+        Assert.AreEqual(3, small);
+        Assert.AreEqual(8, medium);
+        Assert.AreEqual(15, large);
+        Assert.AreEqual(25, xl);
     }
 }
