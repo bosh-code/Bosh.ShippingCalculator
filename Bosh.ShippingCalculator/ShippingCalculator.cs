ef3a24e15a5356ff5dded04c29f0b35e1cb2365d
bosh-code <ryan.julian.bosher@icloud.com>
2022-02-21 13:35:52 +1300

    change GetPrice to allow for speedyShipping

--- Bosh.ShippingCalculator/ShippingCalculator.cs
+++ Bosh.ShippingCalculator/ShippingCalculator.cs
@@ -4,7 +4,7 @@ public static class ShippingCalculator
 {
     public static int GetPrice(int size, bool speedyShipping)
     {
-        return size switch
+        var price = size switch
         {
             <= 0 => throw new ArgumentException("Size must be greater than 0"),
             < 10 => 3,
@@ -12,5 +12,6 @@ public static class ShippingCalculator
             < 100 => 15,
             >= 100 => 25
         };
+        return speedyShipping ? price * 2 : price;
     }
 }
